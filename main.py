1c5d61881afdfe3ff72a92a77f29ac9c8bdced8b
Asbjørn Alexander Fellinghaug <asbjorn@fellinghaug.com>
2018-02-12 14:57:03 +0100

    Added better msg handling + reponse to incoming connection

--- main.py
+++ main.py
@@ -3,6 +3,10 @@
 import sys
 import socket
 import time
+import json
+from contextlib import contextmanager
+
+import paho.mqtt.client as mqtt
 
 
 HOST = ''
@@ -10,6 +14,7 @@ PORT = 50120
 BUFSIZE = 1024
 ADDR = (HOST, PORT)
 
+DEVICES = json.load(open('devices.json', 'r')).get('devices', [])
 
 class Client:
     def __init__(self, address):
@@ -19,8 +24,15 @@ class Client:
         self.recv_history = []
         self.last_msg = time.time()
 
+    def is_expired(self):
+        now = time.time()
+        if (now - self.last_msg) > 60:
+            return True
+        return False
+
     def received(self, msg):
         self.recv_history.append(msg)
+        self.last_msg = time.time()
 
     def get_last_msg(self):
         if len(self.recv_history) > 0:
@@ -36,10 +48,11 @@ class Client:
 
 
 class UdpServer:
-    def __init__(self, address):
+    def __init__(self, address, callback=None):
         self._sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         self._sock.bind(address)
         self._clients = {}
+        self._callback = callback
 
     def run(self):
         while True:
@@ -52,21 +65,36 @@ class UdpServer:
             if addr not in self._clients:
                 self._clients[addr] = Client(addr)
                 self._clients[addr].received(data)
-                self._clients[addr].send(self.sock, "Welcome new client!")
+                self._clients[addr].send(self._sock, "ACK: Welcome new client!")
                 print("New client registered: {}".format(self._clients[addr]))
+            else:
+                self._clients[addr].received(data)
+                self._clients[addr].send(self._sock, "ACK")
 
+            # Cleanup expired clients..
+            for c_key in self._clients:
+                if self._clients[c_key].is_expired():
+                    self._clients.pop(c_key)
 
+            try:
+                msg = json.loads(data)
+                if self._callback and callable(self._callback):
+                    self._callback(msg)
 
-            #send_data = input("> ")
-            #if send_data is not None:
-            #    encoded_data = str.encode(send_data)
-            #    self._sock.sendto(encoded_data, addr)
+            except json.decoder.JSONDecodeError as err:
+                print("Error decoding JSON: {}".format(err))
+                print("")
 
         self._sock.close()
 
+# @contextmanager
+def mqtt_to_thingsboard(msg):
+    print("debugging: {}".format(msg))
+    device = msg.get("device")
+
 
 def main():
-    server = UdpServer(ADDR)
+    server = UdpServer(ADDR, callback=mqtt_to_thingsboard)
     try:
         server.run()
     except KeyboardInterrupt:
